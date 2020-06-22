603f64f46cbf30ab63aa3bb3d9aae934b5734305
wanglisen1 <1347915884@qq.com>
2020-06-22 07:39:39 +0000

    645665

--- app/Http/Controllers/Controller/StudentController.php
+++ app/Http/Controllers/Controller/StudentController.php
@@ -7,11 +7,29 @@ use App\Model\StudentModel;
 class StudentController extends Controller
 {
 	public function stucode(Request $request){
-        $code = $_GET['code'];//获取code
-		$weixin =  file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid=	wx69cbb9e821210abb&secret=19cddecc89314ab3b31f9976cedc41f6&code=".$code."&grant_type=authorization_code");//通过code换取网页授权access_token
-			$jsondecode = json_decode($weixin); //对JSON格式的字符串进行编码
-			$array = get_object_vars($jsondecode);//转换成数组
-			$openid = $array['openid'];//输出openid
-		return $openid;
-		}
+	$data = request()->post();
+
+    $code = !empty($data['code']) ? $data['code'] : '';
+    //配置appid
+    $appid = !empty($data['appid']) ? $data['appid'] : 'wx69cbb9e821210abb ';
+    //配置appscret
+    $secret = !empty($data['secret']) ? $data['secret'] : '19cddecc89314ab3b31f9976cedc41f6';
+
+    if(empty($code)||empty($appid)||empty($secret)){
+        return $this->getBack('0','非法请求','');
+    }
+
+    //api接口
+    $api = "https://api.weixin.qq.com/sns/jscode2session?appid={$appid}&secret={$secret}&js_code={$code}&grant_type=authorization_code";
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_URL, $api);
+    $res = json_decode(curl_exec($curl),true);
+    curl_close($curl);
+
+    return $this->getBack('1','ok',$res);
 }
