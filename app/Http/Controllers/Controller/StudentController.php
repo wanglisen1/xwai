fd69b85add5a3c3efb5912fecdf8d7fc34afa404
wanglisen1 <1347915884@qq.com>
2020-06-23 03:33:04 +0000

    543543

--- app/Http/Controllers/Controller/StudentController.php
+++ app/Http/Controllers/Controller/StudentController.php
@@ -91,6 +91,7 @@ class StudentController extends Controller
   	//小问模块展示（家长端）
   public function stuxw(Request $request){
   		$this->session();
+  		echo 111111;exit;
   		echo $_SESSION["openid"];
   		$res = StudentModel::where('stu_openid',$_SESSION["openid"])->first();
   		$uid = $res['stu_id'];
